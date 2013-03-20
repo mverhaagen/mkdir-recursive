42f87910c642afc0337367fb1fc4b88367fd4ede
Mike <mverhaagen@icgconsulting.com>
2013-03-20 15:32:08 -0700

    Changed to a more simple approach

--- script/mkdir-recursive.js
+++ script/mkdir-recursive.js
@@ -1,35 +1,17 @@
-var fs = require("fs")
-
-function mkdir_recursive(path, callback, index) {
-	var parts = require("path").normalize(path).split("/");
-	var index = index || 0;
-	var fileMode = 0777;
+(function() {
+	var fs = require('fs')
 	
-	if(index >= parts.length) {
-		return callback()
+	exports.mkdir_recursive = function (path, callback) {
+			var parts = require("path").normalize(path).split("/");
+			var mode = 0777;
+			var path = ""
+			for(var index in parts){
+				path += parts[index] + "/";
+
+				fs.mkdir(path, mode, callback)
+			}
+			callback()
 	}
 	
-	var dir = parts.slice(0, index + 1).join("/");
-	fs.stat(dir, function(error) {
-			if(error === null){
-				mkdir_recursive(path, callback, index + 1)
-			}
-			else {
-				fs.mkdir(dir, fileMode, function(err){
-					if(err != null){
-						if(callback){
-							callback()
-						}
-						else
-						{
-							throw err
-						}
-					}
-					else
-					{
-						mkdir_recursive(path, callback, index + 1)
-					}
-				})
-			}
-	});	
-}
+	
+}).call(this)
